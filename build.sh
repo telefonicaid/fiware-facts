d9907d5e7cc9854b160482969296a62a0b929fb2
Toni Robres <arobres@tid.es>
2014-05-02 12:32:14 +0200

    fix problem in build

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 # to be shown in sonar
 #
 # __author__ = 'arobres'
-redis-server &
-rabbitmq-server &
+
 virtualenv ENV
 source ENV/bin/activate
 mkdir /var/log/fiware-facts
@@ -12,6 +11,4 @@ pip install -r requirements.txt
 pip install -r requirements_dev.txt
 python facts.py &
 nosetests-2.7 -s -v --cover-package=facts --with-cover --cover-xml-file=target/site/cobertura/coverage.xml --cover-inclusive --cover-erase --cover-branches --cover-xml --with-xunit
-kill $(lsof -t -i:6379)
 kill $(lsof -t -i:5000)
-kill $(lsof -t -i:5672)
