4f1140be27c00347c236030fb91d5eb21c982487
Toni Robres <arobres@tid.es>
2014-05-05 11:55:17 +0200

    add new configuration to CI

--- build.sh
+++ build.sh
@@ -11,5 +11,5 @@ chmod 777 /var/log/fiware-cloto
 pip install -r requirements.txt
 pip install -r requirements_dev.txt
 python facts.py &
-nosetests-2.6 -s -v --cover-package=facts --with-cover --cover-xml-file=target/site/cobertura/coverage.xml --cover-inclusive --cover-erase --cover-branches --cover-xml --with-xunit
+nosetests -s -v --cover-package=facts --with-cover --cover-xml-file=target/site/cobertura/coverage.xml --cover-inclusive --cover-erase --cover-branches --cover-xml --with-xunit
 kill $(lsof -t -i:5000)
