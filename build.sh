0e28d7dd28d4095d99a3a2296aa7f8bcb4af4d02
Toni Robres <arobres@tid.es>
2014-05-05 15:45:56 +0200

    delete backslash

--- build.sh
+++ build.sh
@@ -11,5 +11,5 @@ chmod 777 /var/log/fiware-cloto
 pip install -r requirements.txt
 pip install -r requirements_dev.txt
 python facts.py &
-nosetests -s -v --cover-package=facts --with-cover --cover-xml-file=target/site/cobertura/coverage.xml --cover-inclusive --cover-erase --cover-branches --cover-xml --with-xunit --xunit-file=target/surefire-reports//TEST-nosetests.xml
+nosetests -s -v --cover-package=facts --with-cover --cover-xml-file=target/site/cobertura/coverage.xml --cover-inclusive --cover-erase --cover-branches --cover-xml --with-xunit --xunit-file=target/surefire-reports/TEST-nosetests.xml
 kill $(lsof -t -i:5000)
