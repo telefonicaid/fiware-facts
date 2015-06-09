bd4582b5091e68187f34e2e1ccc9f1c4c9ae8b29
Guillermo Jimenez Prieto <e.fiware.tid@telefonica.com>
2015-06-09 14:06:10 +0200

    fix: dev: fixing tests about getting windowsizes from cloto component

--- tests/test_cloto_client.py
+++ tests/test_cloto_client.py
@@ -25,9 +25,24 @@
 __author__ = 'gjp'
 from unittest import TestCase
 from mockito import *
-from mock import patch
-from requests import Response
-from facts.config import config
+from mock import patch, MagicMock
+from keystoneclient.exceptions import NotFound
+
+
+class MyCursor(MagicMock):
+    # Mock of a mysql cursor
+    result = None
+
+    def execute(self, query):
+        # Generates mocked results depending of the MYSQL query content
+        if query.startswith("SELECT * FROM ") \
+                and query.__contains__("cloto.cloto_tenantinfo")\
+                and query.__contains__("WHERE tenantId=\"tenantId\""):
+            self.result = [["tenantId", 5]]
+
+    def fetchall(self):
+        # returns the result of the query
+        return self.result
 
 
 class cloto_client_tests(TestCase):
@@ -36,43 +51,26 @@ class cloto_client_tests(TestCase):
         self.tenantId = "tenantId"
         self.fake_tenantId = "fakeTenantId"
         self.mockedClient = mock()
-        response = Response()
-        response.status_code = 200
-        response._content = '{"owner": "Telefonica I+D", ' \
-            '"doc": "https://forge.fi-ware.org/plugins/mediawiki/wiki/fiware/' \
-            'index.php/Policy_Manager_Open_RESTful_API_Specification",' \
-            ' "runningfrom": "15/04/09 10:37:47",' \
-            ' "version": "1.2.0", "windowsize": 5}'
-        response_failure = Response()
-        response_failure.status_code = 401
-        response_failure._content = '{"unauthorized": ' \
-            '{"message": ' \
-            '"Token is not valid for specified tenant fakeTenantId (HTTP 401)", "code": 401}}'
-        clotoURL = config.get('common', 'cloto')
-        clotoPort = config.get('common', 'clotoPort')
-        clotoVersion = config.get('common', 'clotoVersion')
-        headers = {"Content-type": "application/json", "Accept": "application/json"}
-        when(self.mockedClient).get(clotoURL + ":" + clotoPort + "/" + clotoVersion + "/" +
-                                    self.tenantId, headers=headers).thenReturn(response)
-        when(self.mockedClient).get(clotoURL + ":" + clotoPort + "/" + clotoVersion + "/" +
-                                    self.fake_tenantId, headers=headers).thenReturn(response_failure)
+        mockedCursor = MyCursor()
+        when(self.mockedClient).cursor().thenReturn(mockedCursor)
 
     def test_cloto_client(self):
         """Tests if method gets a window size from fiware-cloto component."""
-        import facts.cloto_client as Cloto
-        cloto = Cloto.cloto_client()
-        cloto.client = self.mockedClient
+        import facts.cloto_db_client as Cloto
+        cloto = Cloto.cloto_db_client()
+        cloto.conn = self.mockedClient
         response = cloto.get_window_size(self.tenantId)
         self.assertEqual(response, 5)
 
-    @patch('facts.cloto_client.logging')
-    def test_cloto_client_fail(self, mock_logging):
+    @patch('facts.cloto_db_client.logging')
+    @patch('facts.cloto_db_client.mysql', )
+    def test_cloto_client_no_connection(self, mock_mysql, mock_logging):
         """Tests if method fails when try to get a window size of a fake tenantId from fiware-cloto component."""
-        import facts.cloto_client as Cloto
-        cloto = Cloto.cloto_client()
-        cloto.client = self.mockedClient
+        import facts.cloto_db_client as Cloto
+        cloto = Cloto.cloto_db_client()
         try:
-            response = cloto.get_window_size(self.fake_tenantId)
-        except SystemError as ex:
+            response = cloto.get_window_size(self.tenantId)
+        except NotFound as ex:
+            self.assertTrue(mock_mysql.connect.called)
             self.assertTrue(mock_logging.error.called)
             self.assertRaises(ex)
