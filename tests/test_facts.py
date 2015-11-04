675a5217387007c1101e44a05ade67cd41280064
Guillermo Jimenez Prieto <geonexus@gmail.com>
2015-11-04 15:01:42 +0100

    new: test: Adding tests to check the use case: exception control to some no-managed situations.

--- tests/test_facts.py
+++ tests/test_facts.py
@@ -145,3 +145,66 @@ class MyTest(MyAppTest):
 
         response = self.app.post(self.url3, data=json.dumps(data2), headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status_code, 200)
+
+    def test_context_broker_empty_attributes_message(self):
+        """ Test that the POST operation over the API returns a 400 error response if message has no attributes.
+
+        :return       200 Ok
+        """
+        data3 = {"contextResponses": [
+                    {
+                        "contextElement": {
+                           "id": "Trento:193.205.211.69",
+                           "isPattern": "false",
+                           "type": "host"
+                       },
+                       "statusCode": {
+                           "code": "200",
+                           "reasonPhrase": "OK"
+                       }
+                   }
+                ]
+            }
+
+        response = self.app.post(self.url3, data=json.dumps(data3), headers={'Content-Type': 'application/json'})
+        self.assertEqual(response.status_code, 400)
+
+    def test_context_broker_any_attribute_is_missing_message(self):
+        """ Test that the POST operation over the API returns a 400 error response if message has no attributes.
+
+        :return       200 Ok
+        """
+        data4 = {"contextResponses": [
+                    {
+                        "contextElement": {
+                           "attributes": [
+                               {
+                                   "value": "0.14",
+                                   "name": "cpuLoadPct",
+                                   "type": "string"
+                               },
+                               {
+                                   "value": "0.856240",
+                                   "name": "freeSpacePct",
+                                   "type": "string"
+                               },
+                               {
+                                   "value": "0.8122",
+                                   "name": "netLoadPct",
+                                   "type": "string"
+                               }
+                           ],
+                           "id": "Trento:193.205.211.69",
+                           "isPattern": "false",
+                           "type": "host"
+                       },
+                       "statusCode": {
+                           "code": "200",
+                           "reasonPhrase": "OK"
+                       }
+                   }
+                ]
+            }
+
+        response = self.app.post(self.url3, data=json.dumps(data4), headers={'Content-Type': 'application/json'})
+        self.assertEqual(response.status_code, 400)
