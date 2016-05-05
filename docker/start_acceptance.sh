25f8a1974429491f99cc65980b3c935d10654b0a
visitor <henar.munozfrutos@telefonica.com>
2016-05-05 07:59:53 +0200

    update start

--- docker/start_acceptance.sh
+++ docker/start_acceptance.sh
@@ -12,4 +12,4 @@ while ! nc -z redis 6379; do sleep 8; done
 while ! nc -z rabbit 5672; do sleep 8; done
 while ! nc -z fiwarecloto 8000; do sleep 8; done
 while ! nc -z fiwarefacts 5000; do sleep 8; done
-behave features/component --tags ~@skip
+behave features/ --tags ~@skip --junit --junit-directory testreport
